43797dadd568bf7067e103ceb3c8fb41fdc50560
TomerNissim <tomernis@gmail.com>
2021-01-30 20:15:54 +0200

    8 tests passed and improved sorting

--- src/main.js
+++ src/main.js
@@ -6,19 +6,22 @@ document.addEventListener("DOMContentLoaded", function() {
     const ul = document.getElementById("list-of-tasks");
     const spanCounter = document.getElementById("counter");
     const sortButton = document.getElementById("sort-button");
-    const updateButton = document.getElementById("update-button");    
+    const updateButton = document.getElementById("update-button");   
+    const sortSelect = document.getElementById("sort-selector");
+    const categorySelector = document.getElementById("category-selector");
     let taskIndex = 1;
     let listOfTasks = [];
     let taskCounter = 0;
-    let lastDeletedLi;
-    let liEditClicked;
+    let lastDeletedLi = null;
+    let liEditClicked = null;
+    let sortedBy = "priority";
+    let bin =  {"my-todo": listOfTasks}
     getFromJsonbin();
     
     updateButton.hidden = true
     spanCounter.innerText = taskCounter ;
     addButton.addEventListener("click",addTask);
     sortButton.addEventListener("click",sortTasks);
-
 
 //  the main functions of the project
 //
@@ -32,19 +35,18 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function displayTask(task){
-        console.log(task)
         const li = createHtmlTags(task);
         task.li = li
         ul.append(li);
         spanCounter.innerText = ++taskCounter;
-        console.log(li);
     }
 
     function createTaskObject(creationDate){
         const task = {priority: prioritySelector.value,
             date: creationDate,
-            description: input.value,
+            text: input.value,
             check: false,
+            category: categorySelector.value,
             index: taskIndex++
         };
         return task;
@@ -58,6 +60,10 @@ document.addEventListener("DOMContentLoaded", function() {
             taskPriority.className = "todo-priority";
             taskPriority.innerText = task.priority;
             taskContainer.append(taskPriority);
+        const taskCategory = document.createElement("span");
+            taskCategory.className = "todo-category";
+            taskCategory.innerText = task.category;
+            taskContainer.append(taskCategory);
         const taskCreationTime = document.createElement("span");
             taskCreationTime.className = "todo-created-at";
             taskCreationTime.innerText = task.date;            
@@ -65,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const taskText = document.createElement("span");
             taskContainer.append(taskText);
             taskText.className = "todo-text";
-            taskText.innerText = task.description;
+            taskText.innerText = task.text;
         const deleteButton = document.createElement("button");
             deleteButton.className = "delete-button";
             deleteButton.innerText = "Delete";
@@ -74,6 +80,18 @@ document.addEventListener("DOMContentLoaded", function() {
             editButton.className = "edit-button";
             editButton.innerText = "Edit";
             taskContainer.append(editButton);
+        switch(task.category){
+            case "general" :
+                taskCategory.className = "general-category";
+                break;
+            case "family":
+                taskCategory.className = "family-category";
+                break;
+            case "work":
+                taskCategory.className = "work-category";
+                break;    
+
+        }  
         editButton.addEventListener("click",taskEdit);
         deleteButton.addEventListener("click",deleteTask);
         updateButton.addEventListener("click",taskUpdate);
@@ -88,16 +106,42 @@ document.addEventListener("DOMContentLoaded", function() {
 //sorting functions
 //       
     function sortTasks(){
-        listOfTasks.sort(sortByPriority);
+        sortedBy = sortSelect.value
+        switch(sortedBy){
+            case "priority":
+                listOfTasks.sort(sortByPriority);
+                break;
+            case "date":
+                listOfTasks.sort(sortByDate);
+                break;
+            case "category":
+                listOfTasks.sort(sortByCategory);
+                break;    
+        }
         for(let i = 0; i < listOfTasks.length; i++){
-            console.log(listOfTasks)
            ul.removeChild(listOfTasks[i].li);
         }
         for(let i = 0; i < listOfTasks.length; i++){
             ul.append(listOfTasks[i].li);
          }
     }
-
+    function sortByCategory(a,b){
+        if(a.category > b.category){
+            return 1;
+        }else if(a.category < b.category){
+            return -1
+        }
+        return 0;
+            
+    }
+    function sortByDate(a,b){
+        if(a.index > b.index){
+            return 1;   
+        }else if(a.index < b.index){
+            return -1;
+        }
+        return 0;
+    }
     function sortByPriority(a,b){
         if(a.priority > b.priority)
             return -1;
@@ -112,22 +156,24 @@ document.addEventListener("DOMContentLoaded", function() {
         const response = /*await*/ fetch('https://api.jsonbin.io/v3/b/6013f80a1de5467ca6bdcbd4',
             { method: 'PUT',
               headers: {'Content-Type': 'application/json','X-BIN-NAME': 'tomer-final-todo-list-project', 'X-Master-Key':'$2b$10$r0N4nxOcMRRmC99AgaIA.uT9Y.1OMVam6H4owoZdPjZ3ruVcBDy6u'},
-              body: JSON.stringify(listOfTasks)});
+              body: JSON.stringify({"my-todo": listOfTasks})});
     }
     
     async function getFromJsonbin(){
         const response = await fetch('https://api.jsonbin.io/v3/b/6013f80a1de5467ca6bdcbd4/latest',
             {method: 'GET',headers: {'Content-Type': 'application/json','X-BIN-NAME': 'tomer-final-todo-list-project', 'X-Master-Key':'$2b$10$r0N4nxOcMRRmC99AgaIA.uT9Y.1OMVam6H4owoZdPjZ3ruVcBDy6u'}});
         if(response.ok){
-            const responseText = await response.text();
-            let responseParse = JSON.parse(responseText);
-            listOfTasks = responseParse.record;
+            const responseText = await response.json();
+            let responseRecord = responseText["record"];
+            listOfTasks = responseRecord["my-todo"];
+            console.log(listOfTasks)
             if(listOfTasks[0] !== false){
                 for(let i =0;i<listOfTasks.length;i++){
                     displayTask(listOfTasks[i]);
                 }
             }
             else listOfTasks = [];
+            taskIndex = listOfTasks[listOfTasks.length - 1].index + 1;
         }          
     }
 
@@ -155,27 +201,38 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function taskEdit(){
-        updateButton.classList.toggle("update-Button");
+        if(liEditClicked !== null){
+            liEditClicked.classList.toggle("markEdit");
+            updateButton.hidden = true;
+        }
         updateButton.hidden = !updateButton.hidden
         let li = this.closest("li");
         li.classList.toggle("markEdit");
         liEditClicked = li;
         let task = getTaskFromListOfTask(li);
-        input.value = task.description;
+        input.value = task.text;
         prioritySelector.value = task.priority;
+        if(updateButton.hidden){
+            input.value = "";
+            prioritySelector.value = 1;
+            liEditClicked = null;
+        }
+
     }
     function taskUpdate(){
         let task = getTaskFromListOfTask(liEditClicked);
-        task.description = input.value;
+        task.text = input.value;
         task.priority = prioritySelector.value;
         updateJsonbinStorage();
         let todoText = liEditClicked.querySelector(".todo-text");
         let todoPriority = liEditClicked.querySelector(".todo-priority");
-        todoText.innerText = task.description;
+        todoText.innerText = task.text;
         todoPriority.innerText = task.priority;
         liEditClicked.classList.toggle("markEdit")
-        console.log(listOfTasks); 
         updateButton.hidden = true;
+        input.value = "";
+        prioritySelector.value = 1;
+        liEditClicked = null;
     }
 
 // etc functions
@@ -194,63 +251,6 @@ document.addEventListener("DOMContentLoaded", function() {
         date = date.replace("T"," ");
         return date;
     }
-
-      //  const task = getTaskFromListOfTask(li);
-      //  editContainer = createHtmlTagsForEdit(task)
-
-    
-
-    
-    // function createHtmlTagsForEdit(task){
-    //     const editContainer = document.createElement("span");
-    //     const editInput = document.createElement("input");
-    //     const editPrioritySelector = document.createElement("select");
-    //     const option1 = document.createElement("option");
-    //         option1.value = "1";
-    //         option1.innerText = "1";
-    //         editPrioritySelector.append(option1);
-    //     const option2 = document.createElement("option");
-    //         option2.value = "2";
-    //         option2.innerText = "2";
-    //         editPrioritySelector.append(option2);
-    //     const option3 = document.createElement("option");
-    //         option3.value = "3";
-    //         option3.innerText = "3";
-    //         editPrioritySelector.append(option3);
-    //     const option4 = document.createElement("option");
-    //         option4.value = "4";
-    //         option4.innerText = "4";
-    //         editPrioritySelector.append(option4);
-    //     const option5 = document.createElement("option");
-    //         option5.value = "5";
-    //         option5.innerText = "5";
-    //         editPrioritySelector.append(option5);    
-    //     const updateButton = document.createElement("button");
-    //     updateButton.className = "update-Button"
-    //     updateButton.innerText = "Update";
-    //     editContainer.append(editInput);
-    //     editContainer.append(editPrioritySelector);
-    //     editContainer.append(updateButton);
-
-        // updateButton.addEventListener("click", updateTask)
-        // return editContainer;
-    // }
-    // editInputTemp;
-    // let editPrioritySelectorTemp;
-
-    // function updateTask(){
-    //     const li = this.closest("li");
-    //     const task = getTaskFromListOfTask(li);
-    //     const newTask = {priority: editPrioritySelector.value,
-    //         date: task.date, 
-    //         description: editInput.value,
-    //         check: false,
-    //         index: taskIndex++
-    //     };
-    //     return task;
-    //     editContainer.remove();
-    // }
-    
 });
 
    
